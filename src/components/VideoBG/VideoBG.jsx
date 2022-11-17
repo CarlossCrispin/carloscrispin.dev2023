ac966187653f8864d8ec827d07b3aa2e23999bf6
carlosscrispin <carlos.crispin.cc@gmail.com>
2022-11-16 18:19:54 -0600

    "fix autoplay"

--- src/components/VideoBG/VideoBG.jsx
+++ src/components/VideoBG/VideoBG.jsx
@@ -5,9 +5,15 @@ const VideoBG = () => {
   return (
     <div className="text-indigo-600 dark:text-slate-300 ">
       <div className="overlay"></div>
-      <video src={videoBG} autoPlay loop muted className="h-screen"></video>
-      <div className="content">
-      </div>
+      <video
+        src={videoBG}
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="h-screen"
+      ></video>
+      <div className="content"></div>
     </div>
   )
 }
